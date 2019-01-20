10ed25eeb15fe13ec4309c32af809392ac26c62f
Guy Morgenshtern <guymorg17@gmail.com>
2019-01-19 21:10:18 -0500

    res

--- client/src/CharacterPanel.java
+++ client/src/CharacterPanel.java
@@ -1,2 +1,62 @@
-public class CharacterPanel {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class CharacterPanel extends JPanel {
+
+    private Game2 window;
+    private JLayeredPane pane;
+    Action action;
+
+    JButton controlScreen;
+    JButton joinGame;
+    JButton createGame;
+    Image background;
+
+    String characterSprite;
+    String carSprite;
+
+
+    CharacterPanel(Game2 window) {
+        this.window = window;
+
+        this.setLayout(new BorderLayout());
+        pane = new JLayeredPane();
+        pane.setBounds(0, 0, 800, 600);
+
+
+        add(pane);
+
+        this.setVisible(true);
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.drawImage(background, 0,0, 800, 600, null);
+
+        repaint();
+    }
+
+    private class Action implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == controlScreen) {
+                window.setControlPanel(new ControlPanel(window));
+                window.changeState(1);
+            } else if (e.getSource() == joinGame) {
+                window.changeState(2);
+            } else if (e.getSource() == createGame) {
+                window.changeState(3);
+            }
+        }
+    }
+
 }
