c4349256350e2bd5f6aae1284cb6344aa3a80592
Yash Arora <yash_arora6@hotmail.com>
2019-01-21 08:07:10 -0500

    Update server/src/Server.java

--- server/src/Server.java
+++ server/src/Server.java
@@ -9,12 +9,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-class ServerRun {
-    public static void main (String[] args) {
-        Server server = new Server("MapOne.txt");
-        server.run();
-    }
-}
 
 public class Server implements Runnable{
     private ServerSocket serverSock;// server socket for connection
