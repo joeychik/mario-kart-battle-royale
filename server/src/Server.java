6b912ecb87a51c90c1d7fb74e2120c6e66646024
joeychik <joey.chik0103@gmail.com>
2019-01-22 01:29:48 -0500

    now automatically enters the gamepanel after server starts game

--- server/src/Server.java
+++ server/src/Server.java
@@ -195,17 +195,17 @@ public class Server implements Runnable{
                         if (map[yMapPosition][xMapPosition] instanceof Marker) {
                             System.out.println("yes");
                             //checks if the player is currently intersecting any remaining markers
-                            for (MapComponent check: p.getMarkerList()) {
-
-                                //if they are, player's markersPassed increments by one
-                                if (p.getHitBox().intersects(check.getHitBox())) {
-                                    System.out.println("yes");
-                                    p.setMarkersPassed(p.getMarkersPassed() + 1);
-
-                                    //removes intersecting marker from arraylist of remaining markers
-                                    p.getMarkerList().remove(check);
-                                }
-                            }
+//                            for (MapComponent check: p.getMarkerList()) {
+//
+//                                //if they are, player's markersPassed increments by one
+//                                if (p.getHitBox().intersects(check.getHitBox())) {
+//                                    System.out.println("yes");
+//                                    p.setMarkersPassed(p.getMarkersPassed() + 1);
+//
+//                                    //removes intersecting marker from arraylist of remaining markers
+//                                    p.getMarkerList().remove(check);
+//                                }
+//                            }
                             //checks if player is over a finish line
                         } else if (map[yMapPosition][xMapPosition] instanceof FinishMarker) {
                             //increase laps completed by one
