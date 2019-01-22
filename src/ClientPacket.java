eda28ec2b3b7d1ba6d6ebf8bd5386f9590b73f82
Guy Morgenshtern <guymorg17@gmail.com>
2019-01-22 11:21:35 -0500

    BARE COMMENTS

--- src/ClientPacket.java
+++ src/ClientPacket.java
@@ -4,6 +4,14 @@ public class ClientPacket extends Packet{
     private double orientation;
     private boolean brake;
 
+
+    /**
+     * Constructor
+     * @param velocity
+     * @param accel
+     * @param orientation
+     * @param brake
+     */
     public ClientPacket(double velocity, double accel, double orientation, boolean brake) {
         this.velocity = velocity;
         this.accel = accel;
@@ -11,18 +19,34 @@ public class ClientPacket extends Packet{
         this.brake = brake;
     }
 
+    /**
+     * getAccel
+     * @return acceleration
+     */
     public double getAccel() {
         return accel;
     }
 
+    /**
+     *
+     * @return brake (true/false)
+     */
     public boolean isBrake() {
         return brake;
     }
 
+    /**
+     * getVelocity
+     * @return velocity
+     */
     public double getVelocity() {
         return velocity;
     }
 
+    /**
+     * getOrientation
+     * @return orientation
+     */
     public double getOrientation() {
         return orientation;
     }
--- src/FinishMarker.java
+++ src/FinishMarker.java
@@ -1,63 +1,121 @@
-import java.awt.*;
+import java.awt.Rectangle;
 
 public class FinishMarker extends MapComponent {
 
+    private boolean driveable;
+    private double dimensions;
+    private int xPosition;
+    private int yPosition;
+    private Rectangle hitBox;
+
+    /**
+     * Constructor
+     * @param dimensions
+     * @param xPosition
+     * @param yPosition
+     * @param driveable
+     */
     FinishMarker(int dimensions, int xPosition, int yPosition, boolean driveable) {
         super(dimensions, xPosition, yPosition, driveable);
+        this.dimensions = dimensions;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.driveable = driveable;
+        hitBox = new Rectangle(xPosition, yPosition, dimensions, dimensions);
     }
 
     FinishMarker() {
         super();
     }
 
+    /**
+     * getDimensions
+     * @return dimension
+     */
     @Override
     public double getDimensions() {
-        return 0;
+        return dimensions;
     }
 
+    /**
+     * setDimensions
+     * @param dimensions
+     */
     @Override
     public void setDimensions(double dimensions) {
 
     }
 
+    /**
+     * getyPosition
+     * @return yPosition
+     */
     @Override
     public int getyPosition() {
-        return 0;
+        return yPosition;
     }
 
+    /**
+     * setyPosition
+     * @param position
+     */
     @Override
     public void setyPosition(int position) {
-
+        this.yPosition = position;
     }
 
+    /**
+     * getxPosition
+     * @return xPosition
+     */
     @Override
     public int getxPosition() {
-        return 0;
+        return xPosition;
     }
 
+    /**
+     * setxPosition
+     * @param position
+     */
     @Override
     public void setxPosition(int position) {
-
+        this.xPosition = position;
     }
 
+    /**
+     * getHitBox
+     * @return Rectangle
+     */
     @Override
     public Rectangle getHitBox() {
-        return null;
+        return hitBox;
     }
 
+    /**
+     * setHitBox
+     * @param hitBox
+     */
     @Override
     public void setHitBox(Rectangle hitBox) {
-
+        this.hitBox = hitBox;
     }
 
+    /**
+     * getDriveable
+     * @return boolean
+     */
     @Override
     public boolean getDriveable() {
-        return true;
+        return driveable;
     }
 
+    /**
+     * setDriveable
+     * @param driveable
+     */
     @Override
     public void setDriveable(boolean driveable) {
-
+        this.driveable = driveable;
     }
 
 }
